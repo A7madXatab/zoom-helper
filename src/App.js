40b3bbfc25e84598f47f7056fad251d2fa87a755
A7madXatab <Ahmadzendane725@gmail.com>
2021-01-13 23:59:31 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    }
+  }
+
+  showFile = async (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = async (e) => {
+      const text = (e.target.result).toString();
+      this.setState({
+        messages: text.split('\n')
+      })
+    };
+    reader.readAsText(e.target.files[0])
+  }
+
+  focusInput = () => {
+    console.log("printed")
+    console.log(document.getElementById('file').focus());
+  }
+
+  render = () => {
+
+    return (<div className="w-full flex items-center justify-center flex-col">
+      <header className="w-full bg-blue-900 px-2 py-10">
+        <nav>
+
+        </nav>
       </header>
+      <div className="w-full md:w-8/12 pt-2">
+        {/* <button onClick={this.focusInput} className="transition duration-200 ease-in px-3 py-1 border border-blue-900 rounded-md hover:bg-blue-900 hover:text-white">Choose File</button> */}
+        <input id="file" type="file" onChange={(e) => this.showFile(e)} />
+        {this.state.messages.map(m => {
+          if (m.split('to')[1] !== undefined)
+          {
+            const indexOfTo = m.indexOf('to');
+            const sender = (m.split('to')[0]).split('From')[1];
+            const reciever = (m.split('to')[1]).split(':')[0]
+
+            return (
+              <div className="rounded-md border p-3 my-4 hover:shadow-md hover:border-blue-900 bg-white">
+
+                <header className="flex">
+                  <h1>
+                    <span className="font-bold">Sender:</span> {sender}
+                  </h1>
+                  <span className="mx-2 font-bold">
+                    To
+                   </span>
+                  <h2>
+                    {reciever}
+                  </h2>
+                </header>
+
+                <main>
+                  <h1>Answer is</h1>
+                  <p>
+                    {m.substring(m.lastIndexOf(':') + 1)}
+                  </p>
+                </main>
+              </div>
+            );
+          }
+
+        })}
+      </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
